0ac67c97ab6fb323657a97fd0b1486f660081142
Syed Aun Mohd <aunmohd280@gmail.com>
2023-03-30 23:31:58 +0530

    commit

--- Voice_Assistant_using_Python.py
+++ Voice_Assistant_using_Python.py
@@ -7,7 +7,7 @@ import webbrowser
 
 engine=pyttsx3.init()
 voices=engine.getProperty('voices')
-engine.setProperty('voice',voices[1].id)
+engine.setProperty('voice',voices[0].id)
 recognizer=sr.Recognizer()
 
 def cmd():
